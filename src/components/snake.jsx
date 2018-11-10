d6ba06d05afe59b3d8f816dbdf5681afe71cdca8
Nancy Zitle <ancizj@hotmail.com>
2018-11-09 20:32:24 -0500

    Implement the setInterval for move snake

--- src/components/snake.jsx
+++ src/components/snake.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './snake.css';
 
+let handleEvent;
 export default class Snake extends Component {
   constructor(props) {
     super(props);
@@ -11,13 +12,32 @@ export default class Snake extends Component {
     window.addEventListener('keydown', this.handleKeyDown);
   }
 
+  componentDidUpdate() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
   componentWillUnmount() {
     /* global window */
     window.removeEventListener('keydown', this.handleKeyDown);
   }
 
+
   handleKeyDown = (event) => {
+
+    let keyCode = event.keyCode;
+    
     console.log(event)
+    let contador = 0;
+    const snake = document.querySelector('.snake');
+    
+    if(keyCode === 39){
+      handleEvent = setInterval(()=>{
+        contador++;
+        snake.style.marginLeft = `${contador*10}px`;
+      },100)
+    }
+    if(keyCode === 32){
+      clearInterval(handleEvent);
+    }
   }
   render() {
     return (

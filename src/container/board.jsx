980cd4261d39254a56bc184de83e442d5d307800
Nancy Zitle <ancizj@hotmail.com>
2019-05-12 06:40:34 -0500

    Limita la area en dónde puede moverse la snake

--- src/container/board.jsx
+++ src/container/board.jsx
@@ -11,7 +11,13 @@ const DIRECTION = {
 
 class Board extends Component {
   state = {
-    board:[[1,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]],
+    board:[
+      [1,0,0,0], // 0 // filas  // co
+      [0,0,0,0], // 1           // lum
+      [0,0,0,0], // 2           // nas
+      [0,0,0,0], // 3
+      [0,0,0,0], // 4
+    ],
     position:{
       x: 0,
       y: 0
@@ -27,33 +33,43 @@ class Board extends Component {
     const { width, height } = section.getBoundingClientRect();
     this.setState({
       size:{
-        width:Math.ceil((width / 4) - 10),
-        height:Math.ceil((height / 4) - 10)
+        width:Math.ceil((width / 5) - 10),
+        height:Math.ceil((height / 5) - 10)
       }
     })
 
     document.addEventListener('keydown',this.handleKeyEvent);
   }
 
-  move = direction =>{
+  move = (direction) => {
     let { position:{x,y}, board } = this.state;
-   
+    const lengthBoard = board.length;
+
     if(direction === DIRECTION.RIGHT){
-      board[y][x] = 0; // resetea la posicion actual
-      x++; // incremente la posicion (según el eje)
-      board[y][x] = 1; // le da un nuevo valor, de acuerdo a la posicion modificada
+      console.log(x)
+      if(x !== (lengthBoard -1)){
+        board[y][x] = 0; // resetea la posicion actual
+        x++; // incremente la posicion (según el eje)
+        board[y][x] = 1; // le da un nuevo valor, de acuerdo a la posicion modificada
+      }
     }else if(direction === DIRECTION.LEFT){
-      board[y][x] = 0;
-      x--;
-      board[y][x] = 1;
+      if(x !== 0){
+        board[y][x] = 0;
+        x--;
+        board[y][x] = 1;
+      }
     }else if(direction === DIRECTION.UP){
-      board[y][x] = 0;
-      y--;
-      board[y][x] = 1;
+      if(y !== 0){
+        board[y][x] = 0; // posicion inicial
+        y--; // el cambio en la posicion
+        board[y][x] = 1;
+      }
     }else if(direction === DIRECTION.DOWN){
-      board[y][x] = 0;
-      y++;
-      board[y][x] = 1;
+      if(y !== (lengthBoard - 1)){
+        board[y][x] = 0;
+        y++;
+        board[y][x] = 1;
+      }
     }
 
     this.setState({
@@ -66,11 +82,11 @@ class Board extends Component {
   }
   
   handleKeyEvent = event =>{
-    this.move(event.keyCode)
+    this.move(event.keyCode, event)
   }
 
   render() {
-    const { board, size, currentCell } = this.state;
+    const { board, size } = this.state;
     const width = `${size.width || 0}px`;
     const height = `${size.height || 0}px`;
 
@@ -90,15 +106,7 @@ class Board extends Component {
               );
             });
           })
-        }
-        {/* 
-          <Snake 
-            width={width}
-            height={height}
-            position
-          />
-        */}
-        
+        }        
       </section>
     );
   }
